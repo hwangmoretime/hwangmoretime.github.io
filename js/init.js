b0b1f448eb7db834a6916fb89075ca4489cf21a7
David Hwang <aviddhwang@gmail.com>
2015-04-07 21:31:00 -0400

    updated androgynouse_names, attribution

--- js/init.js
+++ js/init.js
@@ -1,7 +1,7 @@
 /*
-	TXT by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+	TXT by Pixelarity
+	pixelarity.com @pixelarity
+	License: pixelarity.com/license
 */
 
 (function($) {
@@ -10,7 +10,7 @@
 		reset: 'full',
 		breakpoints: {
 			'global':	{ range: '*', href: 'css/style.css' },
-			'desktop':	{ range: '737-', href: 'css/style-desktop.css', containers: 960, grid: { gutters: 25 } },
+			'desktop':	{ range: '737-', href: 'css/style-desktop.css', containers: 1200, grid: { gutters: 50 } },
 			'1000px':	{ range: '737-1200', href: 'css/style-1000px.css', containers: 960, grid: { gutters: 25 }, viewport: { width: 1080 } },
 			'mobile':	{ range: '-736', href: 'css/style-mobile.css', containers: '100%!', grid: { collapse: true, gutters: 20 }, viewport: { scalable: false } }
 		},
